de8a400bb8657c75729a27773ef9c3e251a0c1d7
aromazyl <zyl2336709@gmail.com>
2017-01-01 17:55:18 +0800

    update

---
+++ my_more.c
@@ -0,0 +1,56 @@
+/*
+ * my_more.c
+ * Copyright (C) 2017 zhangyule <zyl2336709@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+
+#define PAGELEN 24
+#define LINELEN 512
+
+void do_more(FILE*);
+
+int see_more();
+
+int
+main(int argc, char* argv[]) {
+  FILE* fp = NULL;
+  if (argc == 1) do_more(stdin);
+  else
+    for (int i = 1; i < argc; ++i) {
+      fp = fopen(argv[i], "r");
+      if (!fp) exit(1);
+      do_more(fp);
+    }
+  return 0;
+}
+
+void do_more(FILE* file) {
+  char line[LINELEN];
+  int num_of_lines = 0;
+  int reply;
+  while (fgets(line, LINELEN, file)) {
+    if (num_of_lines == PAGELEN) {
+      reply = see_more();
+      if (!reply) break;
+      num_of_lines -= reply;
+    }
+    if (fputs(line, stdout) == EOF) exit(0);
+    num_of_lines ++;
+  }
+}
+
+int see_more() {
+  int c;
+  printf("\033[7m more? \033[m");
+  while ((c = getchar()) != EOF) {
+    if (c == 'q') return 0;
+    if (c == ' ') return PAGELEN;
+    if (c == '\n') return 1;
+  }
+  return 0;
+}
